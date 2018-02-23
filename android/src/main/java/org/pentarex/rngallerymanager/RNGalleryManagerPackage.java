8ca85915c428e6321ce42bb302f4e83bfe72e78d
Facundo Acosta <facundoacosta@Facundos-MacBook-Pro.local>
2018-02-23 17:56:43 +0000

    Fixed Android package

--- android/src/main/java/org/pentarex/rngallerymanager/RNGalleryManagerPackage.java
+++ android/src/main/java/org/pentarex/rngallerymanager/RNGalleryManagerPackage.java
@@ -10,16 +10,20 @@ import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class RNGalleryManagerPackage implements ReactPackage {
+      
+    public RNGalleryManagerPackage() {}
+    
     @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
-      return Arrays.<NativeModule>asList(
-          new RNGalleryManagerModule(reactContext)
-      );
+      List<NativeModule> modules = new ArrayList<>();
+      modules.add(new RNGalleryManagerModule(reactContext));
+      return modules;
     }
 
     // Deprecated RN 0.47
